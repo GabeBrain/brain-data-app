b5e3649b5af4fb66a5846c004f86ab445e247059
Gabriel Hxg <gabriel.gomes@brain.srv.br>
2025-08-13 18:04:44 -0300

    aff

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-# app.py (TESTE 1 - Base)
+# app.py
 import streamlit as st
 
 st.set_page_config(page_title="Teste de Base")
 st.title("✅ Teste de Base no Ar!")
 st.success("Se você está vendo esta mensagem, o ambiente do Streamlit Cloud está funcionando.")
-st.balloons()
